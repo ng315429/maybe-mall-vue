be45fecade49809f7b63918cb05662da4dc2581a
ng315429 <ng315429@gmail.com>
2020-07-30 00:10:50 +0900

    레이아웃 및 메인 목록 ui

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="main-wrap" id="app">
+    <div class="main-area">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.main-wrap {
+  width: 100%;
+  margin: 0 auto;
+  background-color: #f7f7f5;
+  .main-area {
+    width: 1005;
+    max-width: 414px;
+    margin: 0 auto;
+    border: 1px solid $main-color;
   }
 }
 </style>
